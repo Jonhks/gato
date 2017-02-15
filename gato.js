c7bf958928bc08ca968c947134141bbf87a0305f
jonhlab <jonathan@laboratoria.mx>
2017-02-15 12:25:44 -0600

    funcionan nombres

---
+++ gato.js
@@ -0,0 +1,61 @@
+// var jugador1 = prompt("Cual es tu nombre1", "Jugador 1");
+// var jugador2 = prompt("Cual es tu nombre2", "Jugador 2");
+
+// // function myFunction(jugador1,jugador2) {
+// 	var jugador1 = prompt("Cual es tu nombre1", "Jugador 1");
+//     var jugador2 = prompt("Cual es tu nombre2", "Jugador 2");
+// // }
+
+// document.getElementById("demo").innerHTML = jugador1;
+// document.getElementById("demo1").innerHTML = jugador2;
+
+
+
+// function jugadores (){
+//   var jugador1 = prompt("Cual es tu nombre");
+//   document.write(jugador1);	
+//   var jugador2 = prompt("Cual es tu nombre");
+//   document.write(jugador2);
+// }
+// jugadores();
+
+// var jugador1;
+// var jugador2;
+// myFunction(jugador1,jugador2);
+// console.log(jugador1);
+// console.log(jugador2);
+
+// document.getElementById("myD").innerHTML = jugador1;
+
+
+// // alert(jugador1,jugador2);
+// // var jugador1 = prompt("¿Cual es tu nombre?", "Primer jugador");
+// // var jugador2 = prompt("¿Cual es tu nombre?", "Segundo jugador");	
+// document.getElementById("j2").innerHTML = prueba;
+// <p id="myD">que onda?</p>
+// 		<script>
+// 			document.getElementById("myD").innerHTML = jugador1;
+// 		</script>
+
+
+// <!DOCTYPE html>
+// <html>
+// <body>
+
+
+// <p id = "jugador2"></p>
+
+
+// <script>
+// function myFunction() {
+// 	var jugador1 = prompt("Cual es tu nombre1");
+//     var jugador2 = prompt("Cual es tu nombre2");
+//     document.getElementById("jugador1").innerHTML = jugador1;
+//     document.getElementById("jugador2").innerHTML = jugador2;
+// }
+// myFunction();
+
+// </script>
+
+// </body>
+// </html>
